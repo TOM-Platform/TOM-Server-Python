341f388eb0c3776377d962251514ae3c4fc91ce2
Jarren <jarrentoh@outlook.com>
2025-01-11 11:46:20 +0800

    feat(whisper): translate and transcript and send it to the FE

--- Services/testing_service/audio_testing_service.py
+++ Services/testing_service/audio_testing_service.py
@@ -1,7 +1,10 @@
 import base_keys
 from APIs.local_llm.local_translator import translate_text
 from base_component import BaseComponent
+from Services.template_service.template_data_handler import build_template_data
 from Utilities import logging_utility
+
+from ..template_service.template_service import TemplateService
 
 _logger = logging_utility.setup_logger(__name__)
 
@@ -27,8 +30,35 @@ class AudioTestingService(BaseComponent):
             text = raw_data[base_keys.AUDIO_TRANSCRIPTION_DATA]
 
             translated = translate_text(text)
+            self._send_websocket_template_data(text=translated)
+
             _logger.info(
                 "\nEnglish: {text}, Translated: {translated}",
                 text=text,
                 translated=translated,
             )
+
+    def _send_websocket_template_data(
+        self, text: str = "", image: bytes = None, audio_path: str = ""
+    ) -> None:
+        """
+        Sending websocket data to the template scene
+
+        :param text: text to be displayed
+        :param image: bytes of the image
+        :param audio_path: the audio file name without the extension.
+            The audio file should be in the "Assets/Resources/Audio" folder of the Unity Client
+        :return: None
+        """
+        websocket_template_data = build_template_data(
+            text=text, image=image, audio_path=audio_path
+        )
+
+        super().send_to_component(websocket_message=websocket_template_data)
+
+        _logger.info(
+            "Sending Template Data (Text: {text}, Image, Audio: {audio_path}) sent to Template Scene",
+            text=text,
+            image=image,
+            audio_path=audio_path,
+        )
